10123fa20fe1ce2733e327171f0e7142e76b603a
msimbo-coding <SAlmonte@MSIMBO.org>
2021-01-29 12:26:59 -0500

    google auth set up

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAuKNTTd69QXbBUnh81WN1G5xOKuLxiIVc",
+    authDomain: "salmonte-job-tracker.firebaseapp.com",
+    projectId: "salmonte-job-tracker",
+    storageBucket: "salmonte-job-tracker.appspot.com",
+    messagingSenderId: "385368107635",
+    appId: "1:385368107635:web:6f6cece5ca5c345f585806"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- sign-in.js
+++ sign-in.js
@@ -1,47 +1,47 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyANjB7fqIVWOQLynDoAc1r5WMMVxMZNRXU",
-    authDomain: "job-tracker-v3.firebaseapp.com",
-    projectId: "job-tracker-v3",
-    storageBucket: "job-tracker-v3.appspot.com",
-    messagingSenderId: "238980648154",
-    appId: "1:238980648154:web:695f9fa9ce095267bea9e3",
-    measurementId: "G-NYFKMG9TFY"
-  };
+  // // Your web app's Firebase configuration
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyANjB7fqIVWOQLynDoAc1r5WMMVxMZNRXU",
+  //   authDomain: "job-tracker-v3.firebaseapp.com",
+  //   projectId: "job-tracker-v3",
+  //   storageBucket: "job-tracker-v3.appspot.com",
+  //   messagingSenderId: "238980648154",
+  //   appId: "1:238980648154:web:695f9fa9ce095267bea9e3",
+  //   measurementId: "G-NYFKMG9TFY"
+  // };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  // // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
 
-  const auth = firebase.auth();
+  // const auth = firebase.auth();
 
-  function signUp(){
+  // function signUp(){
 
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+  //   var email = document.getElementById("email");
+  //   var password = document.getElementById("password");
 
-    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
+  //   const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+  //   promise.catch(e => alert(e.message));
 
-    alert("Signed Up");
+  //   alert("Signed Up");
 
-  }
+  // }
 
-  function signIn(){
+  // function signIn(){
 
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+  //   var email = document.getElementById("email");
+  //   var password = document.getElementById("password");
 
-    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
+  //   const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+  //   promise.catch(e => alert(e.message));
 
-    alert("Signed In" + email);
+  //   alert("Signed In" + email);
 
-  }
+  // }
 
-  function signOut(){
+  // function signOut(){
 
-    auth.signout();
+  //   auth.signout();
 
-    alert("Signed Out");
+  //   alert("Signed Out");
 
-  }
+  // }
