b1d0f84d9e93cf62792000734cbc3e2a7fd7a49d
msimbo-coding <SAlmonte@MSIMBO.org>
2021-01-29 21:04:18 -0500

    updayes

--- search.js
+++ search.js
@@ -49,34 +49,34 @@ jobFilter.forEach(function (job) {
     const data = await result.json();
   
     return data;
-    // console.log(result)
+    console.log(result)
   
-    //@TODO: query API for jobs
-    // returns a dummy object
+    @TODO: query API for jobs
+    returns a dummy object
   
-    // jobs.push({
-    //   salary_min: 50000,
-    //   longitude: -0.776902,
-    //   location: {
-    //     __CLASS__: "Adzuna::API::Response::Location",
-    //     area: ["UK", "South East England", "Buckinghamshire", "Marlow"],
-    //     display_name: "Marlow, Buckinghamshire"
-    //   },
-    //   salary_is_predicted: 0,
-    //   description: `${jobString} Corporate loremeenene ine iendedind eindeidn iend iedneidne deoid ed`,
-    //   title: `${jobString}`,
-    //   category: {
-    //     __CLASS__: "Adzuna::API::Response::Category",
-    //     label: "IT Jobs",
-    //     tag: "it-jobs"
-    //   },
-    //   id: "129698749",
-    //   salary_max: 55000,
-    //   company: {
-    //     __CLASS__: "Adzuna::API::Response::Company",
-    //     display_name: "Corporate Project Solutions"
-    //   }
-    // });
+    jobs.push({
+      salary_min: 50000,
+      longitude: -0.776902,
+      location: {
+        __CLASS__: "Adzuna::API::Response::Location",
+        area: ["UK", "South East England", "Buckinghamshire", "Marlow"],
+        display_name: "Marlow, Buckinghamshire"
+      },
+      salary_is_predicted: 0,
+      description: `${jobString} Corporate loremeenene ine iendedind eindeidn iend iedneidne deoid ed`,
+      title: `${jobString}`,
+      category: {
+        __CLASS__: "Adzuna::API::Response::Category",
+        label: "IT Jobs",
+        tag: "it-jobs"
+      },
+      id: "129698749",
+      salary_max: 55000,
+      company: {
+        __CLASS__: "Adzuna::API::Response::Company",
+        display_name: "Corporate Project Solutions"
+      }
+    });
   
-    // return sampleData;
+    return sampleData;
   }
